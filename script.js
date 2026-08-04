2dfeadd62beacc82e48b2f140550874dfe0a2766
icstvdisplay <icstvdisplay@gmail.com>
2026-08-04 12:06:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
-const image=document.getElementById("imagePlayer");
-const video=document.getElementById("videoPlayer");
+const image = document.getElementById("imagePlayer");
+const video = document.getElementById("videoPlayer");
 
-const runningText=document.getElementById("runningText");
-
-runningText.innerHTML="Welcome to Indonesian Creative School";
+const runningText = document.getElementById("runningText");
+runningText.innerHTML =
+"Welcome to Indonesian Creative School";
 
 function updateClock(){
+    document.getElementById("clock").innerHTML =
+    new Date().toLocaleTimeString();
+}
+setInterval(updateClock,1000);
+updateClock();
 
-const now=new Date();
+let playlist=[];
+let index=0;
 
-document.getElementById("clock").innerHTML=
-now.toLocaleTimeString();
+async function loadPlaylist(){
+
+    const res=await fetch("data/playlist.json");
+
+    playlist=await res.json();
+
+    playItem();
 
 }
 
-setInterval(updateClock,1000);
+function playItem(){
 
-updateClock();
+    if(index>=playlist.length)
+        index=0;
 
-image.style.display="block";
+    const item=playlist[index];
 
-image.src="https://picsum.photos/1920/1080";
+    image.style.display="none";
+    video.style.display="none";
+
+    if(item.type==="image"){
+
+        image.src=item.src;
+        image.style.display="block";
+
+        setTimeout(()=>{
+            index++;
+            playItem();
+        },item.duration*1000);
+
+    }else{
+
+        video.src=item.src;
+        video.style.display="block";
+
+        video.play();
+
+        video.onended=()=>{
+
+            index++;
+            playItem();
+
+        };
+
+    }
+
+}
+
+loadPlaylist();
