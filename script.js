6bcec44b182f2a8b91c203f70c303e295578e799
icstvdisplay <icstvdisplay@gmail.com>
2026-08-05 15:01:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,33 @@ updateClock();
 
 // Cukup diperbarui setiap menit
 setInterval(updateClock, 60000);
+
+
+// =========================
+// COUNTDOWN
+// =========================
+
+const eventDate = new Date("2026-08-17T00:00:00");
+
+function updateCountdown() {
+
+    const now = new Date();
+    const diff = eventDate - now;
+
+    if (diff <= 0) {
+        document.getElementById("countdown").innerHTML =
+            "🇮🇩 Happy Independence Day";
+        return;
+    }
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML =
+        `🇮🇩 Independence Day : ${days} Days ${String(hours).padStart(2,'0')}:${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')}`;
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
